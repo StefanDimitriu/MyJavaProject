ee8a5dbc0ce86c0cdcae2b25bd0e5b0da36057d8
radu <radutache76@gmail.com>
2018-07-13 05:53:31 -0700

    Pe Branch separat

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Direct branch");
+		System.out.println("Direct branch modified a bit");
 	}
 
 }
